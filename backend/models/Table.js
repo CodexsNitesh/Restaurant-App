d92d55dda113bb785d395bf35db24e39e48f6e1b
Nitesh <158025662+MegXux@users.noreply.github.com>
2026-03-25 17:52:46 +0530

    dine in progress

--- backend/models/Table.js
+++ backend/models/Table.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const tableSchema = new mongoose.Schema({
-  tableNumber: { type: String, required: true, unique: true },
+  tableNumber: {
+  type: String,
+  required: function () {
+    return this.orderType === 'dine-in';
+  }
+},
   capacity: { type: Number, default: 4 },
   isActive: { type: Boolean, default: true },
 }, { timestamps: true });
